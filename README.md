46201059ff3671b318afbb139b9c26a3597aa87f
Ludovic Maitre <lmaitre@mediametrie.fr>
2015-04-20 10:42:12 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# OlTools - Online.fr Tools
+
+## Overview
+
+A small utility to get informations on nodes on [Online.fr](https://console.online.net/) via the API they provide.
+
+## Installation
+
+This utility requires nodejs and npm. Install them for your platform.
+
+Then, proceed to install : checkout the project with git and run :
+
+    sudo npm install -g
+
+The default store file is located at $HOME/.oltools/store.json
+
+## Help
+
+  Usage: oltools \[options\] \[command\]
+
+  Commands:
+
+    tokens                   list tokens
+    install <token>          install one token in the store
+    remove <token>           remove one token from the store
+    refresh                  refresh org and servers for all the tokens)
+    generate <scope> <type>  generate for the given scope ("all" or a name) and type ("ansible" or "dns")
+    get <scope> [type]       get the infos for the given scope ("all" or a name) and type ("org" or "server")
+
+  Options:
+
+    -h, --help           output usage information
+    -V, --version        output the version number
+    -s, --store <store>  use the specified store
+    -v, --verbose        verbose mode
+
---
+++ index.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+var path = require('path');
+var pkg = require(path.join(__dirname, 'package.json'));
+
+var program = require('commander');
+
+program.version('1.0.0')
+		.option(
+				'-s, --store <store>',
+				'use the specified store',
+				(process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + "/.oltools/store.json" )
+		.option(
+				'-v, --verbose',
+				'verbose mode' );
+
+var OlTools = require('./lib/oltools.js');
+
+var oltools = new OlTools();
+
+octools.store(program.store);
+if (program.verbose) {
+	console.log('Store is ' + oltools.store);
+}
+
+program.command('tokens').description('list tokens').action(function() {
+		oltools.tokens();
+	});
+
+program.command('install <token>').description(
+		'install one or more tokens in the store').action(function(token) {
+			if ( program.verbose )
+				console.log('install "%s"', token);
+			oltools.addToken( token );
+	});
+
+program.command('remove <token>').description(
+		'remove one token from the store').action(function(token) {
+			if ( program.verbose )
+				console.log('remove "%s"', token);
+			oltools.removeToken( token );
+	});
+
+program.command('refresh').description(
+			'refresh org and servers for all the tokens)').action(function() {
+			if ( program.verbose )
+				console.log('refresh');
+			oltools.refresh();
+	});
+
+program.command('generate <scope> <type>').description(
+	'generate for the given scope ("all" or a name) and type ("ansible" or "dns")').action(function(scope,type) {
+		if ( program.verbose )
+			console.log('generate "%s" "%s"', scope,type);
+		oltools.generate(scope,type);
+	});
+
+program.command('get <scope> [type]').description(
+			'get the infos for the given scope ("all" or a name) and type ("org" or "server")').action(
+			function(scope,type) {
+				if ( program.verbose )
+					console.log('get "%s"%s',scope, ( type != null ? " of type " + type : ""));
+				oltools.get(scope,type);
+			});
+
+program.command('dump <resource> [scope]').description(
+'dump full information for the given resource for all or the specified token)').action(function( resource, scope ) {
+	if ( program.verbose )
+		console.log('dump %s', resource);
+	oltools.dump( resource, scope );
+});
+
+program.parse(process.argv);
